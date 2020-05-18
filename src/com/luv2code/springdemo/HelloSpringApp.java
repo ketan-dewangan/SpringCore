7ce12c5e295693dc05fed261966b7636e6e28ffa
Ketan D <ketand9916@gmail.com>
2020-05-18 23:42:21 +0530

    HelloSpringApp

---
+++ src/com/luv2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,26 @@
+package com.luv2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+		
+		// load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+		
+		
+		// Retrieve bean from spring container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		
+		// call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		
+		// close the context
+		context.close();
+
+	}
+
+}
