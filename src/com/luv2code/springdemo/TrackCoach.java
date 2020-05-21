d9208091170b32ea5096df29b9fbbb7da7b4a1ba
Ketan D <ketand9916@gmail.com>
2020-05-21 22:01:46 +0530

    Life Cycle

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -8,20 +8,10 @@ public class TrackCoach implements Coach {
 		
 	}
 	
-	
-	
-	
-	
-
 	public TrackCoach(FortuneService fortuneService) {
 
 		this.fortuneService = fortuneService;
 	}
-
-
-
-
-
 
 	@Override
 	public String getDailyWorkout() {
@@ -34,5 +24,21 @@ public class TrackCoach implements Coach {
 		
 		return "Track Coach" + fortuneService.getFortune();
 	}
-
+	
+	//add an init method
+	public void doMyStartupStuff() {
+		System.out.println("TrackCoach: Inside method doMyStartupStuff()");
+	}
+	// add a destroy method
+	public void doMyCleanupStuffYoYo(){
+		System.out.println("TrackCoach: Inside method doMyCleanupStuffYoYo()");
+	}
+	
+	
 }
+
+
+
+
+
+
